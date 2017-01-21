1e5afcbb837b32b317d2e4ca5fd5d92f67e090da
Michael Pattie <mjpattie@gmail.com>
2017-01-20 21:38:30 -0800

    Added ground check.

--- GlobalGameJam2017/Assets/PlayerMoveControl.cs
+++ GlobalGameJam2017/Assets/PlayerMoveControl.cs
@@ -6,10 +6,13 @@ public class PlayerMoveControl : MonoBehaviour {
 
 	public float m_walkSpeed;
 	public float m_jumpVelocity;
+	public Transform m_groundCheck;
+	public float m_width;
 
 	int m_walkDirection = 1;
 	Rigidbody2D m_rb;
 	bool m_touchDetected;
+	bool m_grounded;
 	Vector3 m_touchVector;
 
 	// Use this for initialization
@@ -19,6 +22,11 @@ public class PlayerMoveControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		Collider2D groundCheck = Physics2D.OverlapBox(m_groundCheck.position, new Vector2(m_width*2, 0.0001f), 0.0f);
+
+		m_grounded = groundCheck != null;
+
 		m_rb.velocity = new Vector3( m_walkSpeed * m_walkDirection, m_rb.velocity.y);
 
 //		if(Input.GetKeyDown(0)){
@@ -31,20 +39,22 @@ public class PlayerMoveControl : MonoBehaviour {
 
 				m_touchVector = Input.mousePosition;
 				//m_touchDetected = true;
-				Debug.Log("press");
+				
 
 		}
 
 		if(Input.GetMouseButtonUp(0)){
 			
-			Debug.Log("release");
+
 			//Touch touch = Input.GetTouch(0);
 			Vector3 diff = Input.mousePosition - m_touchVector;
-			Debug.Log("magnitude: "+diff.magnitude+" , x: "+Mathf.Abs(diff.x)+", y: "+ Mathf.Abs(diff.y));
+
 			if(Mathf.Abs(diff.x) > Mathf.Abs(diff.y) && diff.magnitude > 100){
 				m_walkDirection = (int)(diff.x/Mathf.Abs(diff.x));
 			}else{
-				m_rb.velocity = new Vector3( m_rb.velocity.x, m_jumpVelocity);
+				if(m_grounded){
+					m_rb.velocity = new Vector3( m_rb.velocity.x, m_jumpVelocity);
+				}
 			}
 				//m_touchDetected = false;
 			
